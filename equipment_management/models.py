3e95fee0797881cee9deec40c1cae95da318a872
kaydurgu <mzhanbolot@gmail.com>
2024-05-19 11:57:36 +0300

    New fields added for equipmen(Equipment types choices changed , status field for equipmenent addedd -----  bugs fixed

--- equipment_management/models.py
+++ equipment_management/models.py
@@ -1,19 +1,24 @@
 from django.db import models
 from django.conf import settings
-
+from workers.models import Profile
 class Equipment(models.Model):
     EQUIPMENT_TYPES = (
-        ('robot', 'Industrial Robot'),
-        ('machine', 'Manufacturing Machine'),
-        ('qc', 'Quality Control System'),
+        ('terminal', 'Terminal'),
+        ('coffee_machine', 'Coffee Machine'),
+        ('force_meter', 'Force Meter'),
+        ('game_console', 'Game Console'),
     )
-
+    STATUS_CHOICES = (
+        ('working', 'Working'),
+        ('under_repair', 'Under Repair'),
+        ('in_storage', 'In Storage'),
+    )
     type = models.CharField(max_length=100, choices=EQUIPMENT_TYPES)
     model = models.CharField(max_length=100)
     installation_date = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=50, blank=True, null=True)
-    added_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, related_name='added_equipment')
-    responsible = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, related_name='responsible_equipment')
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, blank=False)
+    added_by = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, related_name='added_equipment')
+    responsible = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, related_name='responsible_equipment')
 
     def __str__(self):
         return f"{self.get_type_display()} - {self.model}"
@@ -42,8 +47,8 @@ class Alerts(models.Model):
     timestamp = models.DateTimeField()
     description = models.TextField()
     severity = models.CharField(max_length=20, choices=SEVERITY_CHOICES, default='low')
-    detected_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, related_name='detected_alerts')
-    resolved_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, related_name='resolved_alerts', blank=True)
+    detected_by = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, related_name='detected_alerts')
+    resolved_by = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, related_name='resolved_alerts', blank=True)
 
     def __str__(self):
         return f"Alert for {self.equipment} at {self.timestamp}"
